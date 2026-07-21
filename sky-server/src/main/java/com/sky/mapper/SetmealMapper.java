4e42d7a329e45d55f74102efc61870bf83201175
the_sky <2476785771@qq.com>
2026-07-21 15:28:30 +0800

    商品浏览业务功能开发

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -5,10 +5,13 @@ import com.sky.annotation.AutoFill;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface SetmealMapper {
@@ -63,4 +66,14 @@ public interface SetmealMapper {
      * @return
      */
     Integer countByDishIdAndStatus(Long id, Integer enable);
+
+    /**
+     * 动态条件查询套餐
+     * @param setmeal
+     * @return
+     */
+    List<Setmeal> list(Setmeal setmeal);
+
+    @Select("select sd.name,sd.copies,d.image,d.description  from setmeal_dish sd left join dish d on sd.dish_id = d.id where sd.setmeal_id=#{id} ")
+    List<DishItemVO> getDishItemBySetmealId(Long id);
 }
